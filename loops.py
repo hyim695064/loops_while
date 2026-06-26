2582669daf5cac60e1e6f19474ecc3af62e206f1
moshe <hyim695064@gmail.com>
2026-06-26 16:39:25 +0300

    friday2

--- loops.py
+++ loops.py
@@ -6,9 +6,18 @@
 # while x > 0:
 #     print(x)
 #     x -= 1
-total = 0
-x = 1
-while x <= 10:
-    total = total + x
+# total = 0
+# x = 1
+# while x <= 10:
+#     total = total + x
+#     x += 1
+# print(total)
+items = [2, 4, 6, 8]
+x = 0
+while x < len(items):
+    num = items[x]
+    if num > 5:
+        print(num)    
+        break
     x += 1
-print(total)
+
