9a85eaa09ffeb777a15947cc16c0cd1a4e778356
moshe <hyim695064@gmail.com>
2026-06-26 16:25:33 +0300

    friday1

---
+++ loops.py
@@ -0,0 +1,14 @@
+# x = 1
+# while x < 6:
+#     print(x)
+#     x += 1
+# x = 10 
+# while x > 0:
+#     print(x)
+#     x -= 1
+total = 0
+x = 1
+while x <= 10:
+    total = total + x
+    x += 1
+print(total)
